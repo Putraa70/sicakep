2bbc20a3fcbea142c4fe6a9fe3260f21528ef068
sulthon abdul hakim <151292159+slabkim@users.noreply.github.com>
2025-06-18 23:38:26 +0700

    update logout yang sebelumnya tidak bisa, dan mengupdate session pada dashboard

--- views/auth/logout.php
+++ views/auth/logout.php
@@ -1,13 +1,6 @@
 <?php
-// logout.php - Halaman untuk logout pengguna
-
-require_once dirname(__DIR__, 2) . '/config/session.php'; // Pastikan file session.php dimuat
-
-// Hapus semua session data
-session_unset();  // Menghapus semua data session
-session_destroy();  // Menghancurkan session
-
-// Redirect ke halaman login setelah logout
-header('Location: login.php');  // Arahkan pengguna ke halaman login
-exit; // Menghentikan eksekusi script setelah redireksi
+session_start(); 
+session_destroy(); 
+header('Location: login.php');  
+exit;
 ?>
